20a253c046aedf5c61d9d72db70399cf51b08928
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2023-05-27 10:51:00 +0300

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from 'pages/home/home';
+import About from 'pages/about/about';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   );
-};
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import App from 'components/app';
+import 'modern-normalize/modern-normalize.css';
+import './styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/selflovespace">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
